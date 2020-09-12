053738721a9b7d693600d9b0729bbcabe72e0aa6
varun172121 <nuravkumar172121@gmail.com>
2020-09-12 18:29:21 +0530

    improved

--- js/com.js
+++ js/com.js
@@ -1,4 +1,7 @@
-var food=['varun','tirth','yash','foodzilla','autocomplete'];
+var food=['Chhole Bhatur','Bharwa Bhindi','Pindi Chana','Masala Chai','Samosa','Kulche',
+'Panipuri/Golgappe/Phuchka','Jalebi','Aloo gobi','Aloo tikki','Aloo matar','Aloo methi','Aloo shimla mirch','Amritsari kulcha',
+'Biryani','Butter chicken','Chana masala','Sohan papdi','Sukhdi','Upmaa','Vada pav','Ghebar or Ghevar',
+'Maghaz','Khakhra','Laddu'];
 $(document).ready(function(){
     $('#recipe_finder').autocomplete(
         { 
