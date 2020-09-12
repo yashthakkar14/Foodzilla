672e48e70fafd4c40240a8ed3fc2a72fda2fa850
varun172121 <nuravkumar172121@gmail.com>
2020-09-12 21:53:35 +0530

    update

---
+++ material.js
@@ -0,0 +1,9 @@
+// const button = document.querySelector(".foo-button");
+// mdc.ripple.MDCRipple.attachTo(button);
+// mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+const button = document.querySelector('.foo-button');
+mdc.ripple.MDCRipple.attachTo(button);
+
+
+const secondButton = document.querySelector('.bar-button');
+mdc.ripple.MDCRipple.attachTo(secondButton);
