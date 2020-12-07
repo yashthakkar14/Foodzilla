d01594cf21c0c37e4ac3ffc07bf9b56f28c4fe08
AceAltair13 <tirththoria13@hotmail.com>
2020-12-07 16:56:28 +0530

    Updated Django (HTML and Cleanup)

--- Django Website/foodzilla/asgi.py
+++ Django Website/foodzilla/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for first_djangoapp project.
+ASGI config for foodzilla project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_djangoapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodzilla.settings')
 
 application = get_asgi_application()
--- js/register.js
+++ js/register.js
@@ -31,12 +31,12 @@ function a() {
     var flag_2 = 0;
     if ((ans == false) && (password.length <= 8) && (flag == 0)) {
         console.log(flag);
-        alert("password doesnot statisfy the given condition");
+        alert("Password does not statisfy the given condition!");
         flag_1 = 1;
     }
     else if ((repassword != password) && (flag_1 == 0)) {
         console.log(flag_1);
-        alert("password doesnot match");
+        alert("Password does not match!");
     }
     else {
         console.log(flag_1);
