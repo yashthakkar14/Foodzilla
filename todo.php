78ff7caf680be4a9af02f05b79258621228a3603
AceAltair13 <tirththoria13@hotmail.com>
2020-12-17 00:54:14 +0530

    Fixed fatal error

--- todo.php
+++ todo.php
@@ -17,36 +17,31 @@ if (isset($_SESSION["email"])) {
   header("location: login.php");
 }
 
-?>
-<?php 
-    $errors = "";
+$errors = "";
+$uidquery = mysqli_query($conn, "SELECT `uid` FROM users WHERE `email` = '$mail'");
+$row = mysqli_fetch_assoc($uidquery);
+$uid = $row['uid'];
 
-    //connect to the database
-    $db = mysqli_connect('localhost','root','','foodzilla');
-    $uid = mysqli_query($db,"SELECT `uid` FROM users WHERE `email` = '$_SESSION[email]'");
-    
-
-    if(isset($_POST['submit'])){
-        $task = $_POST['task'];
-        if (empty($task)) {
-            $errors = "You must fill in the task";
-        }
-        else{
-            mysqli_query($db,"INSERT INTO tasks(task,`uid`) VALUES ($task,$uid)");
-            header('location:todo.php');
-        }
-    }
+if (isset($_POST['submit'])) {
+  $task = $_POST['task'];
+  if (empty($task)) {
+    $errors = "You must fill in the task";
+  } else {
+    mysqli_query($conn, "INSERT INTO tasks(`task`,`uid`) VALUES ('$task',$uid)");
+    header('location:todo.php');
+  }
+}
 
 
 //Delete a task
-    if(isset($_GET['del_task'])){
-       $id = $_GET['del_task']; 
-       mysqli_query($db,"DELETE FROM tasks WHERE id=$id AND `uid` = $uid");
-       header('location:todo.php');
-    }
-    $tasks = mysqli_query($db,"SELECT * FROM tasks WHERE `uid` = '$uid' ");
-    
-    
+if (isset($_GET['del_task'])) {
+  $id = $_GET['del_task'];
+  mysqli_query($conn, "DELETE FROM tasks WHERE id = $id AND `uid` = $uid");
+  header('location:todo.php');
+}
+
+$tasks = mysqli_query($conn, "SELECT id, task FROM tasks WHERE `uid` = $uid");
+
 ?>
 
 <!DOCTYPE html>
@@ -63,8 +58,8 @@ if (isset($_SESSION["email"])) {
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/dashboard.css" rel="stylesheet">
-  <link rel = "stylesheet" type = "text/css" href = "css/todo.css">
+  <link href="./css/dashboard.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./css/todo.css">
 
 </head>
 
@@ -76,46 +71,45 @@ if (isset($_SESSION["email"])) {
     <!-- Page Content -->
     <div id="page-content-wrapper">
       <?php require_once 'accountnavbar.php' ?>
-      
+
       <div class="todo">
-      <div class="todo_heading">
-        <h2>To - Do</h2>
-    </div>
+        <div class="todo_heading">
+          <h2>To - Do</h2>
+        </div>
 
-    <div class = "content">
-    <form method = "POST" action="todo.php" class = "formholder">
-    <?php if(isset($errors)){ ?>
-        <p class = "todo_errors"> <?php echo $errors; ?> </p>
-    <?php } ?>
-        <input type="text" name = "task" class = "task_input" placeholder = "Input your task">
-        <button type = "submit" class = "add_btn" name = "submit">Add task</button>
-    </form>
+        <div class="content">
+          <form method="POST" action="todo.php" class="formholder">
+            <?php if (isset($errors)) { ?>
+              <p class="todo_errors"> <?php echo $errors; ?> </p>
+            <?php } ?>
+            <input type="text" name="task" class="task_input" placeholder="Input your task">
+            <button type="submit" class="add_btn" name="submit">Add task</button>
+          </form>
 
-    <table class = "todo_table">
-        <thead class = "todo_heading">
-            <th>#</th>
-            <th>Task</th>
-            <th>Action</th>
-        </thead>
+          <table class="todo_table">
+            <thead class="todo_heading">
+              <th>#</th>
+              <th>Task</th>
+              <th>Action</th>
+            </thead>
 
-        <tbody class = "todo_list">
-        <?php $i = 1; 
-        while($row = mysqli_fetch_array($tasks))
-        { ?>
-
-            <tr>
-                <td class = "task_num"> <?php echo $i;?></td>
-                <td class = "task"><?php echo $row['task']?></td>
-                <td class = "delete_task">
-                <a href="todo.php?del_task=<?php echo $row['id'];?>">x</a>
-                </td>
-            </tr>
-        <?php $i++; } ?>
-        </tbody>
-    </table>
-    </div>
+            <tbody class="todo_list">
+              <?php $i = 1;
+              while ($row = mysqli_fetch_array($tasks)) { ?>
+                <tr>
+                  <td class="task_num"> <?php echo $i; ?></td>
+                  <td class="task"><?php echo $row['task'] ?></td>
+                  <td class="delete_task">
+                    <a href="todo.php?del_task=<?php echo $row['id'] ?>">X</a>
+                  </td>
+                </tr>
+              <?php $i++;
+              } ?>
+            </tbody>
+          </table>
+        </div>
       </div>
-    
+
     </div>
 
     <!-- /#page-content-wrapper -->
@@ -124,19 +118,16 @@ if (isset($_SESSION["email"])) {
   <!-- /#wrapper -->
 
   <!-- Bootstrap core JavaScript -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-    </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
-    </script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+  </script>
 
   <!-- Menu Toggle Script -->
   <script>
-    $("#menu-toggle").click(function (e) {
+    $("#menu-toggle").click(function(e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
